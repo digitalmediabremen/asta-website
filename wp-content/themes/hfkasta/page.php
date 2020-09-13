07e988cc27e5de02eba28d58bc2f561eab098b4a
Dustin Sherman <dustin.sherman@yahoo.de>
2020-09-13 19:22:07 +0200

    Notes ins Sidebar updated (mobile display under content), updated menu (footer & sidebar/main), link styling

--- wp-content/themes/hfkasta/page.php
+++ wp-content/themes/hfkasta/page.php
@@ -5,17 +5,13 @@
 <div class="wrapper">
     <main>
         <div class="content">
-            <?php
-                $backgroundBoxes = get_field('background_boxes');
-                $i = 1;
-                if ( $backgroundBoxes ) : 
-            ?>
-                <?php foreach( $backgroundBoxes as $backgroundBox ): ?>
-                    <div class="backgroundbox backgroundbox_0<?php echo $i; ?>"></div>
-                <?php 
-                    $i++;
-                    endforeach; 
-                ?>
+            <?php if (get_field('background_boxes') !== 'no-boxes') : ?>
+                <div class="<?php the_field('background_boxes') ?>">
+                    <div class="backgroundbox backgroundbox_01"></div>
+                    <div class="backgroundbox backgroundbox_02"></div>
+                    <div class="backgroundbox backgroundbox_03"></div>
+                    <div class="backgroundbox backgroundbox_04"></div>
+                </div>
             <?php endif; ?>
 
             <h1><?php the_title(); ?></h1>
@@ -32,9 +28,35 @@
                 </div>
             <?php endif; ?>
 
+            <?php $parentID = $post->post_parent; ?>
+
+            <?php if (get_field('sidebar_note') || get_field('sidebar_note', $parentID)) : ?>
+                <div class="to_content_note">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/arrow_down.svg">
+                </div>
+            <?php endif; ?>
+
             <section>
                 <?php the_content(); ?>
             </section>
+
+            <?php if (get_field('sidebar_note')) : ?>
+                <div class="content_note" id="content_note">
+                    <p>
+                        <?php the_field('sidebar_note'); ?>
+                    </p>
+                </div>
+            <?php else : 
+                $parentID = $post->post_parent;
+            ?>
+                <?php if (get_field('sidebar_note', $parentID)) : ?>
+                    <div class="content_note" id="content_note">
+                        <p>
+                            <?php the_field('sidebar_note', $parentID); ?>
+                        </p>
+                    </div>
+                <?php endif; ?>
+            <?php endif; ?>
         </div>
     </main>
 
--- wp-content/themes/hfkasta/search.php
+++ wp-content/themes/hfkasta/search.php
@@ -5,8 +5,12 @@
 <div class="wrapper">
     <main>
         <div class="content">
-            <div class="backgroundbox backgroundbox_01"></div>
-            <div class="backgroundbox backgroundbox_02"></div>
+            <div class="box-layout-00">
+                <div class="backgroundbox backgroundbox_01"></div>
+                <div class="backgroundbox backgroundbox_02"></div>
+                <div class="backgroundbox backgroundbox_03"></div>
+                <div class="backgroundbox backgroundbox_04"></div>
+            </div>
 
             <?php
                 global $wp_query;
@@ -24,19 +28,37 @@
                 if ( have_posts() ) {
                     ?>
                         <section class="results">
-                    <?php
-                        while ( have_posts() ) {
-                            the_post();
-                            ?>
-                                <a href="<?php the_permalink(); ?>" class="search-result-link">
-                                    <h3><?php the_title(); ?></h3>
-                                    <span><?php the_permalink(); ?></span>
-
-                                    <!--<p><?php // echo the_content(); ?></p>-->
-                                </a>
                             <?php
-                        }
-                    ?>
+                                while ( have_posts() ) {
+                                    the_post();
+                                    ?>
+                                        <a href="<?php the_permalink(); ?>" class="search-result-link">
+                                            <h3><?php the_title(); ?></h3>
+                                            <p>
+                                                <?php
+                                                    $text_padding = 100;
+                                                    $key_word = get_search_query();
+                                                    $str = wp_strip_all_tags(get_the_content());
+                                                    $str_pos = stripos($str, get_search_query());
+
+                                                    $str_start = ($str_pos - $text_padding < 0) ? 0 : stripos($str, ' ', $str_pos - $text_padding);
+                                                    $str_end = ($str_pos + $text_padding > strlen($str)) ? strlen($str) :  stripos($str, ' ', $str_pos + $text_padding);
+
+                                                    $result = substr($str, $str_start, $str_end);
+                                                    
+                                                    $result = substr($result, 0, $text_padding * 2 + strlen($key_word));
+                                                    $result = substr_replace($result, '<strong>', ($str_pos - $str_start), 0);
+                                                    $result = substr_replace($result, '</strong>', (($str_pos - $str_start) + strlen($key_word) + strlen('<strong>')), 0);
+
+                                                    echo $result.'...';
+                                                ?>
+                                            </p>
+                                            <span><?php the_permalink(); ?></span>
+                                        </a>
+                                    <?php
+                                }
+                            ?>
+                            <?php wp_reset_postdata(); ?>
                         </section>
                     <?php
                 }
