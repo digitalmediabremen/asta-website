13716c1e6d39f29ce92dfe4a08e0394cf222cade
jelko arnds <moin@jelkoarnds.com>
2020-09-15 14:32:51 +0200

    Theme moved to submodule.

--- wp-content/themes/hfkasta/searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="search-bar">
-        <input type="search" id="search-bar" class="search-field" placeholder="Suche" value="<?php echo get_search_query(); ?>" name="s" />
-	</label>
-	<input type="submit" class="search-submit" value="" />
-	<img src="<?php echo get_template_directory_uri(); ?>/assets/search.svg" class="search-icon">
-</form>
